d15943e9e524daa55c2976aab07da7057aaccf12
Alex <byczko2012@mail.ru>
2021-03-01 12:22:16 +0300

    Updates

---
+++ precache-manifest.94ba4dc08130b6046f60cfad23066479.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6a335c5a9ae038b77ed06b6d3c3b335",
+    "url": "/SocialNetwork/index.html"
+  },
+  {
+    "revision": "72016e77b9ecffec6a3c",
+    "url": "/SocialNetwork/static/css/main.6811ca18.chunk.css"
+  },
+  {
+    "revision": "31f25f26a07abd307ffb",
+    "url": "/SocialNetwork/static/js/2.f1a78b93.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SocialNetwork/static/js/2.f1a78b93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72016e77b9ecffec6a3c",
+    "url": "/SocialNetwork/static/js/main.d2a45532.chunk.js"
+  },
+  {
+    "revision": "740a19a1f4b6dd886d81",
+    "url": "/SocialNetwork/static/js/runtime-main.d5a6d924.js"
+  },
+  {
+    "revision": "984381ca77db27cd5428627fe933b508",
+    "url": "/SocialNetwork/static/media/Preloader.984381ca.gif"
+  },
+  {
+    "revision": "a3a35b22a7a32e71b6ef7e6a62c303c2",
+    "url": "/SocialNetwork/static/media/users.a3a35b22.png"
+  }
+]);
